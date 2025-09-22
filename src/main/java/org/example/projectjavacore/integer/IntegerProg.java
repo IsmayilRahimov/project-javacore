acff7b5d5d3e2ffd828a8c8a9b6843e03b477c1a
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-09-23 02:31:09 +0400

    String StringBuilde or differency

---
+++ src/main/java/org/example/projectjavacore/integer/IntegerProg.java
@@ -0,0 +1,11 @@
+package org.example.projectjavacore.integer;
+
+public class IntegerProg {
+    public static void main(String[] args) {
+        String a = "123";
+        int num = Integer.parseInt(a);
+        System.out.println(num);
+//        System.out.println(a == b);
+//        System.out.println(a.equals(b));
+    }
+}
