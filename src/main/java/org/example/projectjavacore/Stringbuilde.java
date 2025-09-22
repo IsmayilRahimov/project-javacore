d25cd052ec1728875c10f664863362dc1ec24ba7
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-09-23 01:07:01 +0400

    String StringBuilde or differency

---
+++ src/main/java/org/example/projectjavacore/Stringbuilde.java
@@ -0,0 +1,21 @@
+package org.example.projectjavacore;
+
+public class Stringbuilde {
+    public static void main(String[] args) {
+        String s = "Ismayil";
+        s = s + " Ragimov ";
+        System.out.println(s);
+        s.concat(" Ragimov A");
+
+        StringBuilder sb = new StringBuilder("Masha");
+        sb.append(" Ayi");
+        System.out.println(sb);
+
+
+        String a = "Java";
+
+        a = a.concat(" is the best programming language");
+        System.out.println(a);
+
+    }
+}
