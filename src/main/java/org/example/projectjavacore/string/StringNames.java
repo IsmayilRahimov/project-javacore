94a3af641b5b6e5ad48da2b0047dc18c1598281a
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-09-25 01:11:10 +0400

    String StringBuilde or differency

---
+++ src/main/java/org/example/projectjavacore/string/StringNames.java
@@ -0,0 +1,14 @@
+package org.example.projectjavacore.string;
+
+public class StringNames {
+    public static void main(String[] args) {
+        String name = "Ragimov";
+        System.out.println(name.charAt(0));
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.length());
+        System.out.println(name.substring(0, 5));
+
+
+    }
+}
