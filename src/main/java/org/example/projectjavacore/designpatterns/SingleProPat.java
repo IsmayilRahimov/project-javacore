18ddc734beefad08afa84e459ae53ee2cc89cc6d
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-10-01 19:11:34 +0400

    design patterns 1.Singleton pattern
    */
    /*

--- src/main/java/org/example/projectjavacore/designpatterns/SingleProPat.java
+++ src/main/java/org/example/projectjavacore/designpatterns/SingleProPat.java
@@ -1,18 +1,29 @@
 package org.example.projectjavacore.designpatterns;
 
 public class SingleProPat {
+    // klas daxilinde ozunun static obyektini saxlayiriq
     private static SingleProPat instance;
-    private SingleProPat() {
+
+    private SingleProPat() {  // private constructor = basga yerden yeni obyekt yaradila bilmesin
     }
 
-    public static SingleProPat getInstance() {
+    //  Klassın obyekti yalnız bu metod vasitəsilə əldə olunur
+    public static SingleProPat getInstance() { //
         if (instance == null) {
             instance = new SingleProPat();
         }
-        return instance;
+        return instance;  // eks halda  movcud obyekti qaytar
     }
 
     public void show() {
         System.out.println("SingleProPat show");
+    }
+
+    public static void main(String[] args) {
+        SingleProPat s1 = SingleProPat.getInstance();
+        SingleProPat s2 = SingleProPat.getInstance();
+        s1.show();
+
+        System.out.println(s1 == s2);
     }
 }
