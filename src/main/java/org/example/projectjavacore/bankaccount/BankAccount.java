248c47a836d6860a10bafca51b83f1da1ed60f4e
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-10-01 18:19:33 +0400

    MultiThread practice
    */
    /*

---
+++ src/main/java/org/example/projectjavacore/bankaccount/BankAccount.java
@@ -0,0 +1,23 @@
+package org.example.projectjavacore.bankaccount;
+
+public class BankAccount {
+
+    private double balance;
+
+    public synchronized void withdraw(String threadName, double amount) {
+
+        if (balance >= amount) {
+            System.out.println(threadName + " emeliyyati ishe dushdu :  ");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            balance -= amount;
+            System.out.println(threadName + " emeliyyati bitdi  :  Balans -  " + balance);
+            System.out.println("---------------------------------------");
+        } else {
+            System.out.println("Emeliyyat ishe dushmedi: Balans kifayet etmir: ");
+        }
+    }
+}
---
+++ src/main/java/org/example/projectjavacore/bankaccount/MainAccount.java
@@ -0,0 +1,7 @@
+package org.example.projectjavacore.bankaccount;
+
+public class MainAccount {
+    public static void main(String[] args) {
+
+    }
+}
