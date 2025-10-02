dfd035c139e575fd1ba8b942af965aea2631be73
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-10-02 12:59:09 +0400

    weakHash//
    */
    /*

---
+++ src/main/java/org/example/projectjavacore/arraylist/WeakHash.java
@@ -0,0 +1,24 @@
+package org.example.projectjavacore.arraylist;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.WeakHashMap;
+
+public class WeakHash {
+    public static void main(String[] args) {
+        Map<Object, String> m1 = new HashMap<>();
+
+        Object o1 = new Object();
+        m1.put(o1, "Hash Map");
+        o1 = null;
+        System.gc();
+        System.out.println("HashMap after gc" + m1);
+
+        WeakHashMap<Object, String> m2 = new WeakHashMap<>();
+        Object o2 = new Object();
+        m2.put(o2, "Weak HashMap");
+        o2 = null;
+        System.gc();
+        System.out.println("WeakHashMap after gc" + m2);
+    }
+}
