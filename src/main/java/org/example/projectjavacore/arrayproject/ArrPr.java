1b4fac5a17206307b8db33f38517993e0976e0b9
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-10-11 18:35:04 +0400

    String
    */
    /*

---
+++ src/main/java/org/example/projectjavacore/arrayproject/ArrPr.java
@@ -0,0 +1,19 @@
+package org.example.projectjavacore.arrayproject;
+
+public class ArrPr {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 8};
+
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+    }
+}
