c02cdcc6511f1bf560d2f0ac7d713fe210806c15
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-10-22 02:09:00 +0400

    optional
    */
    /*

---
+++ src/main/java/org/example/projectjavacore/arrayproject/MainPr.java
@@ -0,0 +1,10 @@
+package org.example.projectjavacore.arrayproject;
+
+public class MainPr {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 6};
+        for (int i = 0; i < 10; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ src/main/java/org/example/projectjavacore/optional/NullOptional.java
@@ -0,0 +1,20 @@
+package org.example.projectjavacore.optional;
+
+import java.util.Optional;
+
+public class NullOptional {
+
+    public static void main(String[] args) {
+//        Optional<String> optional = Optional.of("Hello");
+//        System.out.println(optional.isPresent());
+//        System.out.println(optional.get());
+//        Optional<String> empty = Optional.empty();
+//        System.out.println(empty.isPresent());
+//        Optional<String> empty2 = Optional.ofNullable(null);
+//        System.out.println(empty2.isPresent());
+        Optional<String> empty3 = Optional.ofNullable(null);
+        System.out.println(empty3.isPresent());
+        Optional<String> empty4 = Optional.ofNullable("");
+        System.out.println(empty4.isPresent());
+    }
+}
